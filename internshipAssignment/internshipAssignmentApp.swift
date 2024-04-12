2bd3d19d7e6316bbc8d205fdc2aa66c0ca05cec8
NIshitVAts <nishitvats@icloud.com>
2024-04-12 18:02:23 +0530

    Initial Commit

---
+++ internshipAssignment/internshipAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  internshipAssignmentApp.swift
+//  internshipAssignment
+//
+//  Created by Nishit Vats on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct internshipAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
